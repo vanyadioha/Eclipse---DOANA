c0e79d9483c1d95e945390fd24030efae7b49e81
Victor Anyadioha <victoranyadioha@gmail.com>
2023-07-22 21:40:32 +0100

    Feat: Complete Game upload

---
+++ main.py
@@ -0,0 +1,171 @@
+print(
+    """
+
+               ___            %.
+           __  __/__/I__  ______% %%'
+          / __/_[___]/_/I--.   /%%%%
+         / /  I_/=/I__I/  /I  // )(
+        / /____/=/ /_____//  //
+       /  I___/=/ /_____I/  //
+      /______/=/ /_________//
+      I_____/=/ /_________I/MJP
+           /=/_/"""
+)
+print("Welcome Ladies and Gentlemen!")
+print(
+    """
+In the aftermath of the world's silence, where the once bustling cities lie abandoned, and nature reclaims its dominion, a new chapter of humanity unfolds. A solitary figure emerges from the shadows, their determination cutting through the ashen wasteland like a beacon of hope. In a world torn asunder by catastrophe, one question echoes in the hearts of those who remain: Will we crumble, or will we rise? The symphony of survival begins, and the stage is set for an epic tale of courage, sacrifice, and the unyielding human spirit. Welcome to a world beyond the end – Welcome to "Eclipse: Dawn of a New Age."
+"""
+)
+character = input("Choose your Character: Bimbo or Anksh\n")
+pronouns = ""
+pronouns1 = ""
+pronouns2 = ""
+if character.lower() == "anksh":
+    pronouns = "him"
+    pronouns1 = "he"
+    pronouns2 = "his"
+    print(
+        """
+ __)),
+                                //_ _)
+                                ( "\"
+                                 \_-/
+                             ,---/  '---.
+                            /     - -    \
+                           /  \_. _|__,/  \
+                          /  )\        )\_ \
+                         / _/  (   '  ) /  /
+                        / |     (_____) | /
+                       /,'      /     \/ /,
+                     _/(_      (   ._, )-'
+                    `--,/      |____|__|
+                               |    )  |
+                               |   /   |
+                               |  / \  |
+                              / `|  | _)
+                              |  |  |  |
+                              |  /  \  |
+                              | |    \ |
+                              | \    | \_
+                       gnv   /__(    '-._`,
+"""
+    )
+    print("Looking ripped, Anksh!")
+else:
+    pronouns = "her"
+    pronouns1 = "she"
+    pronouns2 = "her"
+    print(
+        """
+  _"_ %
+           (<  ?
+            ` "
+           __||___
+          |\___//_\
+          (' | ') \\
+        __|\ , /  |/
+       /:  /   \  ::
+       \| ######o /|
+         ######## \)
+         ########
+          \ :  /
+           \: /
+            --
+            %%%
+             %%
+              %
+             /:\
+            |/|, b'ger
+"""
+    )
+    print("Looking Peng, Bimbo!")
+
+print("Level 1")
+print(
+    f"In the desolate remnants of civilization, life had become a relentless struggle for survival. Each day was a monotonous battle against scarcity, as hope flickered like a dying ember. But amidst this desolation, a fateful discovery emerged that would change everything. You, {character}, stumbled upon an ancient artifact while scavenging through the ruins of an old library. Dust-covered and seemingly inconsequential, it was a tattered parchment that revealed the enigmatic secrets of a forgotten world. The map to Angkor Wat.\nIn the midst of chaos, the tales of Angkor Wat had become mere myths, whispered around campfires by those yearning for a glimmer of the past. While others implied that it was a sanctuary of abundance, an untouched oasis hidden amidst the destruction others waved it off as an old wives tale. A mere tool for drunken fools to quell their pangs of nostalgia"
+)
+will_you_go = input("Will you go on this adventure? Y or N\n")
+if will_you_go.lower() == "n":
+    print(
+        f"GAME OVER. {character} holds the ancient map of Angkor Wat, staring at it with a mix of longing and uncertainty. The other survivors gather around, awaiting {pronouns} decision. Even though the prospect of exploring a piece of the old world seemed rewarding. {character} opt-ed against looking for angkor wat because there was too much at stake"
+    )
+else:
+    print(
+        f"{character} sits by a flickering fire, surrounded by the weary faces of fellow survivors. The ancient map of Angkor Wat lies before {pronouns}, illuminated by the soft glow of the flames. If there was a chance of finding something or someone out there, {pronouns1} was going to find it. {pronouns1} was going to Angkor wat!"
+    )
+    print("Level 2")
+    print(
+        f"Early the next day, As the sun had barely tipped over the horizon, casting an orange hue across the barren landscape, {character} snuck out of camp and had ridden scorp, {pronouns2} trusty steed for about 13 miles when {character} found {pronouns}self at a crossroad. The ancient map of Angkor Wat lay open before {pronouns}, its faded trails hinting at diverging paths that seemed to promise contrasting destinies. Contemplating the choices ahead, {character}'s eyes traced the winding lines until a realization struck like a bolt of lightning: the roads, though different in appearance, converged further along the journey. It dawned on {pronouns} that no matter which path {pronouns1} chose, the ultimate destination remained the same. Empowered by this revelation, a smile crept onto {character}'s face."
+    )
+    in_what_direction = input("Which road will you take? L or R\n")
+    if in_what_direction.lower() == "r":
+        print(
+            f"GAME OVER. With supposed new found clarity and a feeling of optimism, {character} chose the right road at the crossroads. As the days passed, the path became increasingly treacherous, leading through treacherous terrain and hostile territories. {pronouns1} faced perilous encountered the 'Maraders', the mutant abomination that had risen as a result of the silence. Despite {pronouns2} determination, fate seemed to conspire against {pronouns}. A deadly skirmish with a gang of marauders left {character} mortally wounded. With {pronouns2} last breath, {pronouns1} gazed upon the map clutched in {pronouns2} hand, {pronouns2} final moments consumed by the haunting realization that {pronouns1} had chosen the path of sacrifice, a road that led to an untimely end."
+        )
+    else:
+        print(
+            f"Opting for the left road, {character} felt a surge of hope and determination within. The path unfolded before {pronouns} like a tapestry of possibilities, offering glimpses of a brighter future. As {pronouns1} journeyed onward, the landscape gradually transformed into a realm of beauty and abundance. The road led {pronouns} through thriving landscapes, where nature reclaimed the ruins, and hope blossomed amidst the once-devastated lands. With each passing day, {pronouns2} spirits lifted, and the weight of the past seemed to lift off of {pronouns2} shoulders. Finally, after what felt like an eternity of hardships and resilience, {pronouns1} reached the other side of the crossroads, greeted by the grandeur of lake Amrabat in all its ancient glory. Overwhelmed with optimism, {character} knew that {pronouns2} decision was taking {pronouns} to a place of renewal, a testament to the power of hope and the promise of a new beginning."
+        )
+        print("Level 3")
+        print(
+            f"As if trapped within the realm of a high-stakes game, {character} found {pronouns}self standing at the edge of Lake Amrabat, the crossroads of destiny before {pronouns}. The old boat swayed gently on the dock, tempting {pronouns} with the promise of a solo journey to the other side. On the other hand, Scorp, {pronouns2} loyal companion, stood steadfastly beside {pronouns}, an unwavering symbol of their unbreakable bond. In this pivotal moment, as if guided by an enigmatic game master, a voice echoed through {pronouns2} mind, urging {pronouns} to choose wisely. Which path will {pronouns1} traverse? Shall {pronouns1} abandon {pronouns2} faithful friend and venture alone, or will {pronouns1} face the waters with Scorp by {pronouns2} side? The weight of this choice bore down on {pronouns}, knowing that the consequences of {pronouns2} answer would forever shape the course of {pronouns2} journey."
+        )
+        take_or_leave = input("Take the Boat or Swim across? Boat or Swim\n")
+        if take_or_leave.lower() == "swim":
+            print(
+                f"GAME OVER. Gazing at the old boat, {character} hesitated, {pronouns2} eyes locking with Scorp's, as if the horse understood the gravity of the decision. In an act of unwavering loyalty, {character} chose to forsake the boat and embarked on a daring swim across Lake Amrabat, with Scorp by {pronouns2} side. They pushed through the chilly waters, the currents proving stronger than anticipated. Panic began to set in, and {character} realized {pronouns1} had underestimated the lake's depth and unforgiving tides. With every stroke, fatigue weighed heavily on {pronouns2} limbs, and Scorp struggled to keep pace, their lives intertwined in this desperate battle against nature. In a moment of despair, {character} knew {pronouns1} had made a grave mistake. As exhaustion consumed {pronouns}, {pronouns1} clung to Scorp, finding solace in their shared determination. But it was too late. The lake's unyielding embrace claimed them both, leaving behind a poignant reminder of the consequences that lay in the wake of impulsive choices. The waves lapped against the empty boat on the dock, a haunting testament to the bravery and sacrifice of a bond that would forever be submerged in the depths of Lake Amrabat."
+            )
+        else:
+            print(
+                f"As {character} stood at the edge of Lake Amrabat, {pronouns} felt a mixture of sorrow and determination. Scorp, {pronouns2} loyal companion, nuzzled against {pronouns2} shoulder, sensing {pronouns2} uncertainty. With a heavy heart, {pronouns1} made the agonizing decision to leave the horse behind. {pronouns1.capitalize()} knew that the journey ahead would be too perilous for Scorp, and {pronouns2} faithful friend deserved a chance at survival. Gently stroking Scorp's mane for the last time, {character} bid a tearful farewell and boarded the old boat. Pushing off from the dock, {pronouns1} embarked on a solitary voyage across the vast lake. The waters were choppy and unnerving, but the image of Angkor Wat, the beacon of hope, spurred {pronouns} forward. With each stroke of the oar, {pronouns1} felt the weight of the crossroads' choice fall away, and as {pronouns1} finally reached the other side, {pronouns1} carried the bittersweet memory of Scorp, {pronouns2} valiant companion who had sacrificed so much. Their bond would forever be etched in {pronouns2} heart as {pronouns1} stepped onto the shore, ready to embrace the unknown with newfound strength."
+            )
+            print("Final Level")
+            print(
+                f"With a sense of wonder and awe, {character} stood on the shores of Angkor Wat, the journey across Lake Amrabat behind {pronouns}. Before {pronouns} stood three enchanting doors, each adorned with a distinct color – red, blue, and green. Without a hint of suspicion, {pronouns1} felt a carefree excitement bubbling within {pronouns}, oblivious to the possibility that these doors might conceal unseen dangers. Instinctively drawn to the vivid hues, {pronouns1} made {pronouns2} choice with a lighthearted spirit, trusting that whatever lay beyond would only add to the marvels of this mysterious sanctuary. As {pronouns1} reached out to open the door of {pronouns2} choice, {pronouns1} felt a tingling sense of anticipation, unaware of the unforeseen trials that may lie ahead, and the path that awaited {pronouns} beyond that colorful threshold."
+            )
+            door_dash = input("Which door do we explore? R, G or B\n")
+            if door_dash.lower() == "g":
+                print(
+                    f"GAME OVER. Behind the red door, {character} found {pronouns}self in a maze of twisting passages, each leading to dead ends. The walls seemed to close in around {pronouns}, and an eerie silence enveloped the labyrinth. Panic set in as {pronouns1} realized that the red door had deceived {pronouns}, leaving {pronouns} trapped in an inescapable puzzle. As minutes turned into hours, {character}'s hope began to wane, {pronouns2} once carefree demeanor now overshadowed by the grim realization that {pronouns1} might never find {pronouns2} way out. The relentless maze felt like an endless torment, a taunting reminder of the dangers that lurked behind deceptive facades."
+                )
+            elif door_dash.lower() == "b":
+                print(
+                    f"GAME OVER. Choosing the blue door, {character} was met with an enchanting illusion – a breathtaking paradise with lush greenery and serene waters. Entranced by the idyllic scenery, {pronouns1} stepped forward, only to discover that the landscape was an elaborate mirage. The once inviting waters now revealed their true nature as quicksand, pulling {pronouns} down with each desperate struggle. Gasping for air, {pronouns1} desperately tried to claw {pronouns2} way back to safety, but the mirage held {pronouns} in its deadly grasp. The illusory paradise had become {pronouns2} watery grave, a cruel reminder of how deceit could lead to irreversible consequences."
+                )
+            else:
+                print(
+                    f"As {character} cautiously pushed open the red door, {pronouns1} was greeted by an unexpected sight – an empty room bathed in soft rays of sunlight. The dust-laden air seemed to shimmer as though whispering forgotten tales of the old civilization that once thrived within these walls. In the center of the room, a solitary dusty parchment caught {pronouns2} eye, adorned with what appeared to be a painting from a bygone era. The intricately detailed painting depicted a striking figure, a woman with a serene yet enigmatic smile, her eyes seeming to hold secrets from centuries past.m{character} carefully rolled the parchment and tucked it into {pronouns2} satchel, carrying the echoes of the old civilization."
+                )
+                print(
+                    """
+ ________________________
+ |.----------------------.|
+ ||                      ||
+ ||       ______         ||
+ ||     .;;;;;;;;.       ||
+ ||    /;;;;;;;;;;;\     ||
+ ||   /;/`    `-;;;;; . .||
+ ||   |;|__  __  \;;;|   ||
+ ||.-.|;| e`/e`  |;;;|   ||
+ ||   |;|  |     |;;;|'--||
+ ||   |;|  '-    |;;;|   ||
+ ||   |;;\ --'  /|;;;|   ||
+ ||   |;;;;;---'\|;;;|   ||
+ ||   |;;;;|     |;;;|   ||
+ ||   |;;.-'     |;;;|   ||
+ ||'--|/`        |;;;|--.||
+ ||;;;;    .     ;;;;.\;;||
+ ||;;;;;-.;_    /.-;;;;;;||
+ ||;;;;;;;;;;;;;;;;;;;;;;||
+ ||jgs;;;;;;;;;;;;;;;;;;;||
+ '------------------------'
+"""
+                )
+                art = input(f"What is the name of the art that {character} found?\n")
+                if (art.lower() == "monalisa") or (art.lower("") == "the monalisa"):
+                    print("GAME WON!!! *Curtains fall")
+                else:
+                    print(
+                        "GAME OVER... You tried so hard and got so far but in the end it didn't matter. It was the 'Monalisa'"
+                    )
